433e377dec129fa880de66f13bdda5def763c64e
Lue <lue.amaral@etec.sp.gov.br>
2024-08-30 21:47:03 -0300

    Classe gerenciador de arquivos

---
+++ GerenciarArquivos/GerenciadorArquivos.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjetoEmprestimo.GerenciarArquivos
+{
+    public class GerenciadorArquivos
+    {
+        public static string CadastrarImagemProduto(IFormFile file)
+        {
+            var NomeArquivo = Path.GetFileName(file.FileName);
+            var CaminhoArquivo = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", NomeArquivo);
+
+            using (var stream = new FileStream(CaminhoArquivo, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            return Path.Combine("/Imagens", NomeArquivo).Replace("\\", "/");
+        }
+    }
+}
